b50e115f400ab00fa8384da7318b8dec7e13b26d
root <nnamaka>
2023-04-08 10:58:40 +0000

    new

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+# script to setup server
+
+#sudo apt-get -y update
+#sudo apt-get install -y nginx
+
+# create server directories
+mkdir -p /data/web_static/releases/ /data/web_static/releases/test/
+
+# create fake HTML file
+echo "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html> " | sudo tee /data/web_static/releases/test/index.html
+
+# create a symbolic link 'current' to point to 'test/' folder
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+
+# give ownership to user and group 'ubuntu'
+#sudo chown -R ubuntu:ubuntu /data
+
+# using sed command to updata nginx configuration to serve content to 'hbnb_static'
+sudo sed -i '37 i\ \tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}\n' /etc/nginx/sites-enabled/default
+
+# restart the nginx server
+sudo nginx -s reload
