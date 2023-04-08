9b453c1475d47acf0b966e10a679c608b5336829
root <nnamaka>
2023-04-08 21:40:09 +0000

    see

---
+++ 1-pack_web_static.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+This is python script that archives the content of the web_static
+folder in  a '.tgz' format.
+"""
+
+from datetime import datetime
+from fabric.api import local
+
+# get the current time
+now = datetime.now()
+
+# time string format 
+formatt = "%Y%m%d%H%M%S"
+
+archive_path = "versions/" + "web_static_" + now.strftime(formatt) + ".tgz"
+print("Archive path is {}".format(archive_path))
+
+def do_pack():
+	""" Compresses the web_static folder """
+	result = local("mkdir -p versions && tar -czvf {} web_static/".format(archive_path))
+	
+	if result.failed:
+		print(result)
+		return None
+
+	return archive_path
