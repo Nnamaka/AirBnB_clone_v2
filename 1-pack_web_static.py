2aacf65da02f51461ae347501c4da34d8a744dba
root <nnamaka>
2023-04-09 17:56:48 +0000

    e green

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -19,7 +19,7 @@ print("Archive path is {}".format(archive_path))
 
 def do_pack():
     """ Compresses the web_static folder """
-    command = "mkdir -p versions && tar -czvf {} web_static/"
+    command = "mkdir -p versions && tar -czvf {} web_static/" \
     .format(archive_path)
     result = local(command)
 
