cba81d2f7076f76eccd7dd2f8b56b8884e7490d5
nnamaka <nnamaka7@gmail.com>
2023-04-09 10:54:19 -0700

    green

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -10,18 +10,21 @@ from fabric.api import local
 # get the current time
 now = datetime.now()
 
-# time string format 
+# time string format
 formatt = "%Y%m%d%H%M%S"
 
 archive_path = "versions/" + "web_static_" + now.strftime(formatt) + ".tgz"
 print("Archive path is {}".format(archive_path))
 
-def do_pack():
-	""" Compresses the web_static folder """
-	result = local("mkdir -p versions && tar -czvf {} web_static/".format(archive_path))
-	
-	if result.failed:
-		print(result)
-		return None
 
-	return archive_path
+def do_pack():
+    """ Compresses the web_static folder """
+    command = "mkdir -p versions && tar -czvf {} web_static/"
+    .format(archive_path)
+    result = local(command)
+
+    if result.failed:
+        print(result)
+        return None
+
+    return archive_path
