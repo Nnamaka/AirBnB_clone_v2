e3071486297c7ced707bc1ecf97d7749f89b2bbf
nnamaka <nnamaka7@gmail.com>
2023-04-10 06:13:35 -0700

    message

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+A python script that distributes an archive
+on a webserver(s)
+"""
+
+from fabric.api import local, run, env, put
+import os
+
+# define fab enviroment variables
+env.user = "ubuntu"
+env.hosts = ["3.94.181.72", "54.173.9.45"]
+
+
+def do_deploy(arc_path):
+    """ Upload archive to server """
+    if not os.path.exists(arc_path):
+        return False
+    else:
+        # upload archive to tmp directory
+        put(arc_path, "/tmp/")
+        arc_dest = "/data/web_static/releases/" + \
+            arc_path[9:-4]
+        run("sudo mkdir -p {} && sudo tar -xzvf {} \
+            -C {}/".format(arc_dest, "/tmp/"+arc_path[9:], arc_dest))
+
+        # remove the archive from the temp folder
+        run("sudo rm {}".format("/tmp/"+arc_path[9:]))
+
+        # delete the symbolic link
+        run("sudo mv {}/web_static/* {}".format(arc_dest,
+            arc_dest))
+        run("sudo rm -rf {}/web_static".format(arc_dest))
+        run("sudo rm -rf /data/web_static/current")
+
+        # create symbolic link
+        run("sudo ln -s {} /data/web_static/current".format(
+            arc_dest))
+        return True
