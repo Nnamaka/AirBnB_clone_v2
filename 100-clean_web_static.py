6b7829943bfc76085b330f6c92cfcaa51c76db25
nnamaka <nnamaka7@gmail.com>
2023-04-17 08:14:50 -0700

    its new

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """ Delete out of date archive files"""
 
-# define fab enviroment variables
 env.user = "ubuntu"
 env.hosts = ["3.94.181.72", "54.173.9.45"]
 
@@ -18,15 +17,10 @@ def do_clean(number=0):
 
     path = '/data/web_static/releases'
 
-    # if number == 0 or number == 1:
-    #    number = 1
-    # elif number == 2:
-    #    number = 2
-
-    if number == 0:
+    if number == 0 or number == 1:
+        number = 1
+    elif number == 2:
         number = 2
-    else:
-        number += 1
 
     clean_local = 'ls -t versioins | tail -n +{} | xargs rm -rf'.\
         format(number)
