6da5d461105cda39cc2504a758f7ad7f0a50906a
nnamaka <nnamaka7@gmail.com>
2023-04-17 08:11:38 -0700

    its new

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -18,10 +18,15 @@ def do_clean(number=0):
 
     path = '/data/web_static/releases'
 
-    if number == 0 or number == 1:
-        number = 1
-    elif number == 2:
+    # if number == 0 or number == 1:
+    #    number = 1
+    # elif number == 2:
+    #    number = 2
+
+    if number == 0:
         number = 2
+    else:
+        number += 1
 
     clean_local = 'ls -t versioins | tail -n +{} | xargs rm -rf'.\
         format(number)
