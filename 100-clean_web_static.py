32c32760de1924f275007106d2c150547c9fceea
nnamaka <nnamaka7@gmail.com>
2023-04-17 07:52:18 -0700

    exercise 100

---
+++ 100-clean_web_static.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Delete out of date files
+"""
+
+# define fab enviroment variables
+env.user = "ubuntu"
+env.hosts = ["3.94.181.72", "54.173.9.45"]
+
+
+def do_clean(number=0):
+    """ House Keeping for archived files"""
+
+    if number == 0 or number == 1:
+        number = 1
+    elif number == 2:
+        number = 2
+
+    clean_local = 'ls -t versioins | tail -n +{} | xargs rm -rf'.\
+        format(number)
+
+    clean_server = "ls -t {}| tail -n +{} | xargs rm -rf".format(
+        path, number)
+
+    run(clean_server)
+    local(clean_local)
