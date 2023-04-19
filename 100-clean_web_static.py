2c159a7937cc1301e3679ee25c562c080569e9be
Nnamaka <nnamaka7@gmail.com>
2023-04-19 12:08:50 -0700

    how

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """ Delete out of date archive files"""
+from fabric.api import env, local, run
 
 env.user = "ubuntu"
 env.hosts = ["3.94.181.72", "54.173.9.45"]
@@ -15,3 +16,4 @@ def do_clean(number=0):
     local('cd versions ; ls -t | tail -n +{} | xargs rm -rf'.format(number))
 
     run('cd {} ; ls -t | tail -n +{} | xargs rm -rf'.format(path, number))
+help(do_clean)
