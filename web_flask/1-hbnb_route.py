1d42a505250dccd0e737d93dd9e5e4dc4eb1a85d
Nnamaka <nnamaka7@gmail.com>
2023-04-25 01:58:39 -0700

    added

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -12,10 +12,12 @@ def home_page():
     """" Show a message when accessed """
     return 'Hello HBNB!'
 
+
 @app.route('/hbnb')
 def hbnb_page():
     """ Displays hbnb when accessed """
     return 'HBNB'
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
