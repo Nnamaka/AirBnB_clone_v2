02409191ac5b9b1993eb8d13803b8343868e0e15
Nnamaka <nnamaka7@gmail.com>
2023-04-25 02:50:44 -0700

    how did you

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+""" A script that starts a flask we application """
+from flask import Flask
+
+app = Flask(__name__)
+
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def home_page():
+    """" Show a message when accessed """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hbnb_page():
+    """ Displays hbnb when accessed """
+    return 'HBNB'
+
+
+@app.route('/c/<text>')
+def text_route(text):
+    """ display a custom text """
+    return 'C {}'.format(text.replace('_', ' '))
+
+
+@app.route('/python', defaults={'text'='is cool'})
+@app.route('/python/<text>'):
+    """ Display aa custom text if text is assigned """
+    return 'Python {}'.format(text.replace('_', ' '))
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
